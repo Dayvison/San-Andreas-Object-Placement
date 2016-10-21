bece5959b8f3cfdfa133011748a6eaa92ecbbe7c
Dayvison <dayvisinbalmant@gmail.com>
2016-10-21 17:50:46 -0200

    Fix tuples

--- gen-foliage.py
+++ gen-foliage.py
@@ -64,15 +64,15 @@ def gen_stuff():
 	a = 0
 	b = 0
 	for x in range (-3000, 3000):
-		if a == 1:
-			a = 0
+		if a != 2:
+			a += 1
 			continue
-		a = 1
+		a = 0
 		for y in range (-3000, 3000):
-			if b == 1:
-				b = 0
+			if b != 2:
+				b += 1
 				continue
-			b = 1
+			b = 0
 			c = bitmapdata.colour_at(x, y)
 
 			if c == None:
